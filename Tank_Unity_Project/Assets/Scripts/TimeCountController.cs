ec99ccacf3ee43226e2284377c548fdeb01018f2
VoidMillion <millionvoid@gmail.com>
2020-01-09 19:43:35 +0800

    time count

---
+++ Tank_Unity_Project/Assets/Scripts/TimeCountController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCountController : MonoBehaviour
+{
+    private float startTime;
+    private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        text = GetComponent<Text>();
+        text.text = "Hello world!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = string.Format("Survived for {0} seconds", (int)(Time.time - startTime));
+    }
+}
